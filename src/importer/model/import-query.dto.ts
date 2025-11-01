44cb5749818bdce8df65171e228439314795ba4d
x <ozden.osman54@gmailc.om>
2025-11-01 14:39:15 +0100

    Refactor ImportQueryDto: Remove redundant IsString validation and add Matches rule for countryCode format

--- src/importer/model/import-query.dto.ts
+++ src/importer/model/import-query.dto.ts
@@ -1,13 +1,11 @@
-// ImportQueryDto.ts
-
-import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { IsNotEmpty, Matches, Length } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class ImportQueryDto {
-  @IsString({ message: 'The countryCode must be a string value.' })
   @IsNotEmpty({ message: 'The countryCode query parameter is mandatory. You must provide a country code to proceed with the operation.' })
   @Length(2, 2, { message: 'The countryCode must be exactly 2 characters long, conforming to ISO 3166-1 alpha-2 standard.' })
   @Transform(({ value }) => value.trim()) 
   @Transform(({ value }) => value.toUpperCase()) 
+  @Matches(/^[a-zA-Z]+$/, { message: 'The countryCode must contain only alphabetic characters.' }) 
   countryCode: string;
 }
