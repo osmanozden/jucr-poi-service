927932a3f12a2f430642729538953e7457becfef
x <ozden.osman54@gmailc.om>
2025-11-01 14:40:24 +0100

    Refactor ImportQueryDto: Reorder validation decorators for countryCode to improve readability

--- src/importer/model/import-query.dto.ts
+++ src/importer/model/import-query.dto.ts
@@ -2,10 +2,10 @@ import { IsNotEmpty, Matches, Length } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class ImportQueryDto {
-  @IsNotEmpty({ message: 'The countryCode query parameter is mandatory. You must provide a country code to proceed with the operation.' })
-  @Length(2, 2, { message: 'The countryCode must be exactly 2 characters long, conforming to ISO 3166-1 alpha-2 standard.' })
   @Transform(({ value }) => value.trim()) 
   @Transform(({ value }) => value.toUpperCase()) 
+  @IsNotEmpty({ message: 'The countryCode query parameter is mandatory. You must provide a country code to proceed with the operation.' })
+  @Length(2, 2, { message: 'The countryCode must be exactly 2 characters long, conforming to ISO 3166-1 alpha-2 standard.' })
   @Matches(/^[a-zA-Z]+$/, { message: 'The countryCode must contain only alphabetic characters.' }) 
   countryCode: string;
 }
